2c0b95bc9b376074d53171efdc4706f76bd58c1d
jessicabrinegar <jessicabrinegar@outlook.com>
2023-01-22 20:59:13 +0000

    input field changes styles if input is not valid

--- src/js/component/ListItem.jsx
+++ src/js/component/ListItem.jsx
@@ -1,5 +1,10 @@
 import React from "react";
 
-export default function ListItem ({text}) {
-    return <li className="list-group-item">{text}</li>;
+export default function ListItem ({text, remove}) {
+    return (
+        <div className="d-flex flex-row">
+            <li className="list-group-item">{text}</li>
+            <button onClick={remove}>X</button>
+        </div>
+    )
 }
--- src/js/component/Todos.jsx
+++ src/js/component/Todos.jsx
@@ -1,15 +1,23 @@
-import React, {useState, useEffect} from "react";
+import React, {useState, useEffect, useMemo} from "react";
 import ListItem from "./ListItem.jsx"
 
 
 export default function Todos () {
     const [input, setInput] = useState('');
     const [listItems,setListItem] = useState([]);
+    const [validInput, setValidInput] = useState(true);
 
     const handleAddTodo = (e) => {
         e.preventDefault()
-        setListItem(listItems.concat(<ListItem key={listItems.length} text={input}/>));
-        console.log('list item added')
+        if (input != ''){
+            setRedBorder(false);
+            setListItem(listItems.concat(<ListItem key={listItems.length} text={input} remove={handleRemoveTodo}/>));
+            setInput('');
+            console.log(listItems.length);
+        }
+        else{
+            setValidInput(false);
+        }
     }
 
     const handleRemoveTodo = () => {
@@ -19,9 +27,12 @@ export default function Todos () {
     return (
         <div className="border w-75">
             <form className="d-flex">
-                <input className="px-3 w-100" type="text" placeholder="What needs to be done?" 
+                <input className={`px-3 w-100 ${validInput ? 'valid-item' : 'invalid-input'}`} 
+                type="text" autoFocus 
+                placeholder={validInput ? 'What needs to be done?' : 'Type a todo!'} 
                 onChange={e => setInput(e.target.value)} 
-                value={input}/>
+                value={input}
+                />
                 <button type="submit" onClick={handleAddTodo}>
                     Add
                 </button>
@@ -31,5 +42,5 @@ export default function Todos () {
             </ul>
         </div>
     )
-
 }
+//{`rounded-circle ${color}-light ${activeColor == color ? 'selected' : ''}`}
