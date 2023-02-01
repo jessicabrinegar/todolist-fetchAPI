2bdf19ff6b7154854f8cc3ecd9fa5a9afc6ee89e
jessicabrinegar <jessicabrinegar@outlook.com>
2023-02-01 00:00:25 +0000

    started with API

--- src/js/component/TodoApp.jsx
+++ src/js/component/TodoApp.jsx
@@ -1,4 +1,5 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
+import axios from "axios";
 import Todo from "./TodoItem.jsx"
 
 
@@ -7,12 +8,55 @@ export default function TodoApp () {
     const [todos,setTodos] = useState([]);
     const [validInput, setValidInput] = useState(true);
 
-    const handleAddTodo = (e) => {
+    const getData = async () => {
+        try{
+            const response = await axios.get('https://assets.breatheco.de/apis/fake/todos/user/jessieb');
+            setTodos(response.data);
+            console.log(response.data);
+            console.log(todos);
+        }
+        catch(error){
+            console.log(error.message)
+        }
+        // axios
+        // .get('https://assets.breatheco.de/apis/fake/todos/user/jessieb')
+        // .then(response => setTodos(response.data))
+        //console.log(response.data) --> {"label":"some task", "done":false}
+    }
+    const addData = async () => {
+        try{
+            const response = await axios.put('https://assets.breatheco.de/apis/fake/todos/user/jessieb', [...todos])
+            console.log(response.config.data);
+            setTodos(response.config.data);
+        }
+            // const response = await axios.put('https://assets.breatheco.de/apis/fake/todos/user/jessieb', [...todos, {label:input, done:false}])
+            // todos.length > 0 ? setTodos([...todos, {label:input, done:false}]) : setTodos([{label:input, done:false}]);
+            // setTodos([...todos, {label:input, done:false}])
+        catch(error){
+            console.log(error.message)
+        }
+    }
+        // axios({
+        //     method:'put',
+        //     url:'https://assets.breatheco.de/apis/fake/todos/user/jessieb',
+        //     data: [...todos]
+        // })
+        // .then(response => console.log(response.data));
+    
+    useEffect(() => {
+        getData();
+    }, []);
+
+    useEffect(() => {
+        addData();
+    }, [todos]);
+
+    const handleAddTodo = async (e) => {
         e.preventDefault()
         if (input === '') setValidInput(false);
         else{
             setValidInput(true);
-            todos.length ? setTodos([...todos, input]) : setTodos([input])
+            setTodos([...todos, {label:input, done:false}])
         }
         setInput('');
     }
@@ -21,6 +65,17 @@ export default function TodoApp () {
         let newList = todos.filter((item, i) => i !== index)
         setTodos(newList);
     }
+    const handleRemoveAll = () => {
+        let newData = [{label:'', done:false}];
+        setTodos(newData);
+        axios({
+            method:'put',
+            url:'https://assets.breatheco.de/apis/fake/todos/user/jessieb',
+            data: [{label:'', done:false}]
+        }).then(response => console.log(response.data))
+
+    }
+
     let footerText;
     if (todos.length == 1) footerText = `${todos.length} task left`;
     else if (todos.length == 0) footerText =  'No tasks, add a task';
@@ -39,16 +94,19 @@ export default function TodoApp () {
             </form>
             <ul className="m-0 ps-4 d-flex flex-column align-items-start">
                 {todos.map((todo,index) => {
-                    return <Todo key={index.toString()} 
-                    text={todo} 
+                    return (<Todo key={index.toString()} 
+                    text={todo.label} 
                     index={index} 
                     handleRemoveTodo={handleRemoveTodo}
-                />})}
+                />)})}
             </ul>
             <div className="d-flex flex-row w-100">
-                <li className="list-group-item flex-fill text-start" id="count">
+                <p className="list-group-item flex-fill text-start" id="count">
                     {footerText}
-                </li>
+                </p>
+                <p className="list-group-item flex-fill text-end" id="count">
+                    {todos.length > 1 && <button id="clear-todos" onClick={handleRemoveAll}>Clear all tasks</button>}
+                </p>
             </div>
         </div>
     )
